7fbf7b24d19bf95f8d21920f5c0ba8c92754c0b4
Chris Reed <sydalcr@gmail.com>
2022-07-03 16:46:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const parser = require('body-parser');
+const bodyParser = require('body-parser');
+const { application } = require('express');
+const app = express();
+
+let todos = [];
+let workTodos = [];
+const today = new Date();
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+}
+const day = today.toLocaleDateString('en-US', options);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+   
+    res.render('list', {listTitle: "Home", day: day, todos: todos, postRoute: '/'});
+});
+
+app.post('/', function(req, res){
+
+    todos.push(req.body.addTodo);
+    res.redirect('/');
+})
+
+app.get('/work', function(req, res){
+
+    res.render('list', {listTitle: 'Work', day: day, todos: workTodos, postRoute: '/work'});
+})
+
+app.post('/work', function(req, res){
+
+    workTodos.push(req.body.addTodo);
+    res.redirect('/work');
+})
+
+app.get('/about', function(req, res){
+    res.render('about');
+})
+
+app.listen(3000, function(){
+    console.log('Server started on port 3000');
+});
+
+//TODO -> Add delete functionality to remove item from the list
